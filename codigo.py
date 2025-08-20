9fb20e3f1b3b57ca3be3f5fed680c60ce7278539
Alan Rafael Marquez Hurtado <alanrafamarquez091@gmail.com>
2025-08-20 12:19:16 -0600

    poniedo mi segundo nombre en codigo.py

--- codigo.py
+++ codigo.py
@@ -1,8 +1,9 @@
-nombre = "camila"
+nombre = "Alan Rafael"
 apellido = "Marquez"
 
 print("Hola"+ nombre + " " + apellido + "Como estas?")
 saludo = "Hola"+ nombre + " " + apellido
 
 print(saludo)
+
 
